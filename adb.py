ad693f71855bbfdb83e685045491909f1e377d98
Asuraking913 <israelshedrack913@gmail.com>
2024-02-07 14:56:56 +0100

    New changes

--- adb.py
+++ adb.py
@@ -25,28 +25,38 @@ else:
     print("Failed to retrieve the IP address of your phone.")
 
 def run_adb():
-        system("adb connect {phone_ip}:9999")
+    # return system(f"adb connect {phone_ip}:9999")
+    return system("adb connect 192.168.43.1:9999")
+
 
 def run_scrcpy():
-    system("scrcpy")
+    return system("scrcpy")
         
 def run_tcpip():
-    system("adb tcpip 9999")
+    return system("adb tcpip 9999")
 
 layout = [
         [sg.VPush()],
      [sg.Push(),sg.Text('Welcome to Phone Controller'), sg.Push()],
      [sg.VPush()],
-     [sg.Push(), sg.Button('Connect to Phone', size = (10, 2), font=('young 15 bold')), sg.Push()]
+     [sg.Push(), sg.Button('Connect to Phone', size = (10, 2), font=('young 15 bold'), key='connect'), sg.Button('Restart Port', key='port',visible=False ,size = (10, 2), font=('young 15 bold')), sg.Push()]
      ]
         
 window = sg.Window('Phone Controller', layout, size=(300, 300), font=('young 12'))
 
 while True:
-     event, value = window.read()
+    event, value = window.read()
 
-     if event == sg.WINDOW_CLOSED:
-          break
+    if event == sg.WINDOW_CLOSED:
+        break
+
+    if event == 'connect':
+        connected = run_adb()
+        if connected:
+            run_tcpip()
+        else:
+            sg.popup("""Please plug your phone to your \n computer through a usb cord.\n Please ensure you have Developer \n settings enabled on your phone""")
+        
 
 window.close()
 
